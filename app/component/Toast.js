8b5c71e4898692a1541ee5ec95f53c754518d9b6
nxycdl <nxycdl@163.com>
2017-03-17 17:25:36 +0800

    home

---
+++ app/component/Toast.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react-native';
+import Toast from 'react-native-root-toast';
+
+class Example extends Component{
+    constructor() {
+        super(...arguments);
+        this.state = {
+            visible: false
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => this.setState({
+            visible: true
+        }), 2000); // show toast after 2s
+
+        setTimeout(() => this.setState({
+            visible: false
+        }), 5000); // hide toast after 5s
+    };
+
+    render() {
+        return <Toast
+            visible={this.state.visible}
+            position={50}
+            shadow={false}
+            animation={false}
+            hideOnPress={true}
+            >This is a message</Toast>;
+    }
+}
--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,11 +1,85 @@
-import React,{Component} from 'react';
-import {View, Text} from 'react-native';
+/**
+ * @author Lei
+ * @repo https://github.com/stoneWeb/elm-react-native
+ */
+'use strict';
+
+import React, { Component } from 'react'
+import {
+    Text,
+    View,
+    BackAndroid,
+    ScrollView,
+    StyleSheet,
+    AlertIOS,
+    RefreshControl,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+    TouchableHighlight,
+    Image,
+    TextInput,
+    Platform,
+    TouchableWithoutFeedback,
+    Dimensions,
+    ActivityIndicator,
+    Animated
+    } from 'react-native'
+import LocalImg from '../images'
+import px2dp from '../../util'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+
+import data from '../data'
+
+
+const isIOS = Platform.OS == "ios"
+const { width, height } = Dimensions.get('window')
+const headH = px2dp(isIOS?140:120)
+const InputHeight = px2dp(28)
+
+
 export default class HomePage extends Component {
+
+    componentDidMount(){
+        BackAndroid.addEventListener('hardwareBackPress', function () {
+            BackAndroid.exitApp(0)
+            return true
+        })
+    }
+
+
     render() {
         return (
             <View>
                 <Text>
-                    Home Component Page
+                    header
+                </Text>
+                <Text>
+                    search
+                </Text>
+                <Text>
+                    caption
+                </Text>
+                <Text>
+                    warpper1
+                </Text>
+                <Text>
+                    warpper2
+                </Text>
+                <Text>
+                    best goods List
+                </Text>
+                <Text>
+                    抢购
+                </Text>
+                <Text>
+                    平直优选
+                </Text>
+                <Text>
+                    推荐xxx
+                </Text>
+                <Text>
+                    推荐商家
                 </Text>
             </View>
         )
